9fdc0ee34af3156f04a6012132dfaa10fa52ce5a
Charu Wadhwa <69579664+charu-wadhs16@users.noreply.github.com>
2021-08-25 16:37:28 +0530

    Week 4 Problems

--- Week4/Problem2/Problem2.cpp
+++ Week4/Problem2/Problem2.cpp
@@ -7,16 +7,16 @@ int partition (int arr[], int low, int high)
 	int i = (low - 1); 
 	for (int j = low; j <= high - 1; j++)
 	{
-        c++;
+                c++;
 		if (arr[j] < pivot)
 		{
 			i++;
 			swap(arr[i], arr[j]);
-            s++;
+                        s++;
 		}
 	}
 	swap(arr[i + 1], arr[high]);
-    s++;
+        s++;
 	return (i + 1);
 }
 void quickSort(int arr[], int low, int high)
