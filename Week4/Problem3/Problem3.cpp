516fb55ae8bb315167c9504cdeb8bf5a978b792b
charu-wadhs16 <cwcharu@gmail.com>
2021-09-22 22:27:55 +0530

    Week 4 Problems completed

--- Week4/Problem3/Problem3.cpp
+++ Week4/Problem3/Problem3.cpp
@@ -3,53 +3,33 @@
 #include<climits>
 using namespace std;
 int partition(int arr[], int l, int r, int k);
-int findMedian(int arr[], int n)
-{
-	sort(arr, arr+n); 
-	return arr[n/2]; 
-}
 int kthSmallest(int arr[], int l, int r, int k)
 {
-	if (k > 0 && k <= r - l + 1)
-	{
-		int n = r-l+1; 
-		int i, median[(n+4)/5]; 
-		for (i=0; i<n/5; i++)
-		median[i] = findMedian(arr+l+i*5, 5);
-		if (i*5 < n) 
-		{
-			median[i] = findMedian(arr+l+i*5,n%5);
-			i++;
-		}
-		int medOfMed = (i == 1)? median[i-1]:kthSmallest(median, 0, i-1, i/2);
-		int pos = partition(arr, l, r, medOfMed);
-		if (pos-l == k-1)
-			return arr[pos];
-		if (pos-l > k-1) 
-			return kthSmallest(arr, l, pos-1, k);
-		return kthSmallest(arr, pos+1, r, k-pos+l-1);
-	}
-	return INT_MAX;
+      while(l<=r)
+      {
+        int p=partition(arr,l,r,k);
+        if(p==k-1)
+            return arr[p];
+        else if(p>k-1)
+            return kthSmallest(arr,l,p-1,k);
+        else
+            return kthSmallest(arr,p+1,r,k);
+      }
+      return -1;
 }
 int partition(int arr[], int l, int r, int x)
 {
-	
-	int i;
-	for (i=l; i<r; i++)
-	if (arr[i] == x)
-	break;
-	swap(arr[i],arr[r]);
-	i = l;
-	for (int j = l; j <= r - 1; j++)
-	{
-		if (arr[j] <= x)
-		{
-			swap(arr[i],arr[j]);
-			i++;
-		}
-	}
-	swap(arr[i],arr[r]);
-	return i;
+    int pivot=arr[r];
+    int i=l-1;
+    for(int j=l;j<=r-1;j++){
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[r]);
+    return i+1;
 }
 
 int main()
